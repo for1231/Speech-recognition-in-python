eb990ed83afea6d47e83448f9780eed42e092e08
for1231 <128826043+for1231@users.noreply.github.com>
2023-03-24 20:33:32 +0530

    Update sr.py

--- sr.py
+++ sr.py
@@ -5,6 +5,7 @@ def recognize():
         print("listening...")
         aa=b.listen(mp)
         bb=b.recognize_google(aa)
+        #you can use anything for recognizing the voice like recognize_amazon or recognize_azure
         print(bb)
     return bb
 
